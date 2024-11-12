35db34e42d3fc786f14d42d5bf145116a513a1d3
Blessing767 <blessing.muhwezi@ontariotechu.net>
2024-11-11 23:05:32 -0500

    Update survey.js

---
+++ model/survey.js
@@ -0,0 +1,17 @@
+// models/survey.js
+let mongoose = require('mongoose');
+
+// Define the schema
+let surveySchema = mongoose.Schema({
+    name: String,
+    email: String,
+    yearOfStudy: String,
+    program: String,
+    applicationsLearned: [String],
+    recommendation: Number,
+    additionalComments: String
+}, {
+    collection: "surveys"
+});
+
+module.exports = mongoose.model('Survey', surveySchema);
