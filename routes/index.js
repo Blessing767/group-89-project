8c699440d2951bdcae123e3bc4ce4d4f3815b74f
Blessing767 <blessing.muhwezi@ontariotechu.net>
2024-11-12 17:05:02 -0500

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,6 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const Survey = require('../model/survey');
 
 /* GET index page. */
 router.get('/', function(req, res, next) {
@@ -26,5 +27,16 @@ router.get('/contactus', function(req, res, next) {
   res.render('index', { title: 'Contact us' });
 });
 
+// GET landing page with active and public surveys
+router.get('/', async (req, res) => {
+  try {
+      // Fetch only surveys that are marked as active and public
+      const surveys = await Survey.find({ isActive: true, isPublic: true });
+      res.render('index', { title: 'Home', surveys });
+  } catch (err) {
+      console.error(err);
+      res.render('index', { title: 'Home', surveys: [], error: 'Failed to load surveys' });
+  }
+});
 
 module.exports = router;
